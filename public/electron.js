60f49d45ba3cd02627b58e74b228cf0770ff24ca
TigorHutasuhut <tigor.hutasuhut@gmail.com>
2020-03-28 16:21:02 +0700

    init

---
+++ public/electron.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const isDev = require('electron-is-dev')
+
+let w
+
+const createWindow = () => {
+  w = new BrowserWindow({ minWidth: 600, minHeight: 480 })
+  w.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  )
+  isdev && w.webContents.openDevTools()
+  w.on('closed', () => (w = null))
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => process.platform !== 'darwin' && app.quit())
+
+app.on('activate', () => w === null && createWindow())
